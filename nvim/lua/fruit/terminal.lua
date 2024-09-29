0c77db2830ab67cbf9424edadba5779ba836ecf3
itslychee <itslychee@protonmail.com>
2024-09-29 16:10:42 -0500

    stylua: formatting

--- nvim/lua/fruit/terminal.lua
+++ nvim/lua/fruit/terminal.lua
@@ -1,14 +1,13 @@
 vim.api.nvim_create_autocmd("TermOpen", {
-  pattern = "*",
-  callback = function()
-    local opt = vim.opt_local
-    vim.cmd.startinsert()
-    opt.relativenumber = false
-    opt.number = false
-    opt.signcolumn = "no"
-  end,
+	pattern = "*",
+	callback = function()
+		local opt = vim.opt_local
+		vim.cmd.startinsert()
+		opt.relativenumber = false
+		opt.number = false
+		opt.signcolumn = "no"
+	end,
 })
-
 
 -- https://github.com/mrshmllow/nvim-candy/blob/05bdda5412c3b4842a6cbe86a7f80f7fb9de79e8/candy/lua/marshmallow/autocmd.lua#L5-L10
 vim.api.nvim_create_autocmd({ "VimResized" }, {
