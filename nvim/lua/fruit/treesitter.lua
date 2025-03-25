34acadbb301bfabb81e6a9b41b48ebef37b3c55c
itslychee <itslychee@protonmail.com>
2025-03-25 13:10:45 -0500

    document why ts indent is disabled

--- nvim/lua/fruit/treesitter.lua
+++ nvim/lua/fruit/treesitter.lua
@@ -8,6 +8,24 @@ if success then
             enable = true,
             additional_vim_regex_highlighting = false,
         },
+        -- WHY?:
+        -- Indention is funky for Nix when using `o` after a new element
+        -- e.g.
+        -- mkShell {
+        --  packages = [
+        --     b
+        --               ^[cursor here]
+        --  ]
+        -- }
+        --
+        -- Sources (for me): https://discord.com/channels/1209971237770498088/1209971238349443144/1302388898428878869
+        --
         -- indent = { enable = true },
+        -- is shorthand for
+        -- vim.bo.indentexpr = "nvim_treesitter#indent()"
+        -- use for specific ftplugins if desired
+        --
+        --Source:
+        --https://github.com/nvim-treesitter/nvim-treesitter/issues/6723#issuecomment-2151597595
     })
 end
