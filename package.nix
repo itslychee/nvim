adde8b2eb581a98b4a5cb5d3309f0259911c3784
itslychee <itslychee@protonmail.com>
2025-06-01 18:20:11 -0500

    type shit

--- package.nix
+++ package.nix
@@ -5,6 +5,7 @@
   neovimUtils,
   neovim-unwrapped,
   wrapNeovimUnstable,
+  writeText,
   lib,
   # third party programs
   ripgrep,
@@ -19,65 +20,67 @@
 }:
 let
   inherit (lib) flatten;
-  config = neovimUtils.makeNeovimConfig {
-    plugins = lib.flatten [
-      (builtins.attrValues {
-        inherit (vimPlugins)
-          # nvim-cmp, autocompletion stuffs
-          cmp-async-path
-          cmp-buffer
-          cmp-cmdline
-          cmp-nvim-lsp
-          nvim-cmp
-          # blink-cmp
-          friendly-snippets
-          vim-gnupg
-          # LSPs
-          nvim-lspconfig
-          # Formatting
-          conform-nvim
-          # misc
-          mini-nvim
-          nvim-ts-autotag
-          nvim-lint
-          nvim-ts-context-commentstring
-          # conveniences
-          fidget-nvim
-          lualine-nvim
-          ;
-        inherit extraPlugins;
-        inherit grammars;
-      })
-      (
-        if !debug then
-          vimUtils.buildVimPlugin {
-            name = "fruit-nvim-config";
-            src = ./nvim;
-            doCheck = false;
-          }
-        else
-          [ ]
-      )
-    ];
-
-    luaRcContent =
-      if debug then
-        ''
-          vim.opt.rtp:prepend(vim.uv.cwd() .. "/nvim")
-          require("fruit")
-        ''
+  plugins = lib.flatten [
+    (builtins.attrValues {
+      inherit (vimPlugins)
+        # nvim-cmp, autocompletion stuffs
+        cmp-async-path
+        cmp-buffer
+        cmp-cmdline
+        cmp-nvim-lsp
+        nvim-cmp
+        # blink-cmp
+        friendly-snippets
+        vim-gnupg
+        # LSPs
+        nvim-lspconfig
+        # Formatting
+        conform-nvim
+        # misc
+        mini-nvim
+        nvim-ts-autotag
+        nvim-lint
+        nvim-ts-context-commentstring
+        # conveniences
+        fidget-nvim
+        lualine-nvim
+        ;
+      inherit extraPlugins;
+      inherit grammars;
+    })
+    (
+      if !debug then
+        vimUtils.buildVimPlugin {
+          name = "fruit-nvim-config";
+          src = ./nvim;
+          doCheck = false;
+        }
       else
-        "";
-
-    wrapRc = debug;
-  };
-in
-(wrapNeovimUnstable neovim-unwrapped config).overrideAttrs {
-  runtimeDeps = flatten [
-    extraBinaries
-    # binaries that are convenient :3
-    ripgrep
-    nil
-    nixfmt-rfc-style
+        [ ]
+    )
   ];
-}
+
+  luaRcContent =
+    if debug then
+      ''
+        vim.opt.rtp:prepend(vim.uv.cwd() .. "/nvim")
+        require("fruit")
+      ''
+    else
+      "";
+
+  wrapRc = debug;
+in
+(wrapNeovimUnstable neovim-unwrapped {
+  inherit luaRcContent wrapRc plugins;
+}).overrideAttrs
+  {
+
+    runtimeDeps = flatten [
+      extraBinaries
+      # binaries that are convenient :3
+      ripgrep
+      nil
+      nixfmt-rfc-style
+    ];
+  }
